d387150210e6cd2f89f0ebd8016e9a02b3732ae1
JesperGlas <jesper.glas@gmail.com>
2019-05-10 23:53:17 +0200

    Not sure if i changed anything but pushing just to be sure. Failed to get the movement working.

--- bossethegame.py
+++ bossethegame.py
@@ -2,13 +2,6 @@ import os, sys
 import pygame
 from pygame.locals import *
 
-# exit program
-def events():
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-
 # display settings
 W, H = 800, 800
 HW, HH = W/2, H/2
@@ -29,13 +22,24 @@ GREEN = (124, 252, 0, 255)
 
 # Bosse
 def bosse(x_pos, y_pos):
+    BOSSE_W = W/8
+    BOSSE_H = H/8
     pygame.draw.rect(DS, GREEN, [x_pos, y_pos, W/8, H/8])
+
+# gamestate
+bosse_xpos = 0
+
+def events():
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            pygame.quit()
+            sys.exit()
 
 # game loop
 while True:
     events()
 
-    bosse(0, 0)
+    bosse(bosse_xpos, 0)
 
     pygame.display.update()
     CLOCK.tick(FPS)
