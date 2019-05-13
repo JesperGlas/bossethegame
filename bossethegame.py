559c8ebd3a5eee88ad1562489457e16e2437319e
JesperGlas <jesper.glas@gmail.com>
2019-05-13 19:39:51 +0200

    Added some structure and reusable code for Bosse and Baddies. Having some performance issues. High CPU usage.

--- bossethegame.py
+++ bossethegame.py
@@ -1,18 +1,25 @@
-import os, sys
+import os
+import sys
+import time
 import pygame
 from pygame.locals import *
 
-# display settings
+# settings
 W, H = 800, 800
-HW, HH = W/2, H/2
 AREA = W * H
+W_STEP = W/8
+H_STEP = H/8
+SPEED = 1
 
 # init display
 pygame.init()
-CLOCK = pygame.time.Clock()
 DS = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Bosse the Game")
-FPS = 120
+FPS = 30
+
+# init settings
+CLOCK = pygame.time.Clock()
+START_TIME = 0
 
 # Colors
 BLACK = (0, 0, 0, 255)
@@ -20,48 +27,132 @@ WHITE = (255, 255, 255, 255)
 RED = (255, 0, 0, 255)
 GREEN = (124, 252, 0, 255)
 
-# gamestate
-w_step = W/8
-h_step = H/8
-bosse_xpos = 0
+# Classes
+class Body:
 
-# Body
-def body(x_pos, y_pos, color):
-    body_w = w_step
-    body_h = h_step
-    pygame.draw.rect(DS, color, [x_pos, y_pos, w_step, h_step])
+    def __init__(self, color, width, height, x_pos, y_pos):
+        self.color = color
+        self.width = width
+        self.height = height
+        self.x_pos = x_pos
+        self.y_pos = y_pos
 
-# Bosse
-def bosse(x_pos, y_pos):
-    body(x_pos, y_pos, GREEN)
+    def getWidth(self):
+        return self.width
 
-# Baddie
-def baddie(x_pos, y_pos):
-    body(x_pos, y_pos, RED)
+    def getHeight(self):
+        return self.height
+
+    def moveY(self, y_pos):
+        self.y_pos = y_pos
+
+    def moveX(self, x_pos):
+        self.x_pos = x_pos
+
+    def getY(self):
+        return self.y_pos
+    
+    def getX(self):
+        return self.x_pos
+    
+    def draw(self):
+        drawRect(self.color, self.width, self.height, self.x_pos, self.y_pos)
+
+class Bosse(Body):
+    
+    color = GREEN
+    width = W_STEP
+    height = H_STEP
+
+    def __init__(self, x_pos):
+        return super().__init__(self.color, self.width, self.height, x_pos, (H - H_STEP))
+
+    def moveRight(self, step):
+        if self.x_pos + step < W:
+            self.moveX(self.x_pos + step)
+    
+    def moveLeft(self, step):
+        if self.x_pos - step >= 0:
+            self.moveX(self.x_pos - step)
+
+class Baddie(Body):
+
+    color = RED
+    width = W_STEP
+    height = H_STEP
+    init_y = 0
+
+    def __init__(self, x_pos):
+        return super().__init__(self.color, self.width, self.height, x_pos, self.init_y)
+
+    def move(self):
+        self.y_pos += self.y_pos + 1
+
+# various help methods
+def drawRect(color, width, height, x_pos, y_pos):
+    pygame.draw.rect(DS, color, [x_pos, y_pos, width, height])
+
+# game variables
+bosse = Bosse(0)
+baddies = []
+score = 0
+move_ticks = 0
+last_tick = 0
 
 
+# runtime methods
 def events():
-    global bosse_xpos
+    global bosse
+
 
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
             sys.exit()
-        if event.type == KEYUP:
+        if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 print("Right pressed")
-                if bosse_xpos + w_step < W:
-                    bosse_xpos += w_step
+                bosse.moveRight(W_STEP)
             if event.key == K_LEFT:
                 print("Left pressed")
-                if bosse_xpos - w_step >= 0:
-                    bosse_xpos -= w_step
+                bosse.moveLeft(W_STEP)
+    
+def drawBaddies():
+    global baddies
+
+    for baddie in baddies:
+        baddie.draw()
+
+def moveBaddies():
+    global baddies
+
+    for baddie in baddies:
+        baddie.move()
+
+def checkCollision():
+    global bosse
+    global baddies
+
+    for baddie in baddies:
+        if (baddie.getY() + baddie.getHeight()) >= bosse.getY():
+            if (baddie.getX() + baddie.getWidth()) >= bosse.getX():
+                print("Collision")
+            
 
 # game loop
 while True:
     events()
+    
+    move_ticks = round(pygame.time.get_ticks() / 1000)
 
-    bosse(bosse_xpos, (H - w_step))
+    if move_ticks != last_tick:
+        print("Seconds elapsed: ", move_ticks)
+        print("Score: ", score)
+        last_tick = move_ticks
+
+    bosse.draw()
+    drawBaddies()
+
 
     pygame.display.update()
     CLOCK.tick(FPS)
